2b230abcc42f26d4252358b147d27fdaadad9afb
Adrian <vent.adrian@gmail.com>
2020-07-13 11:25:39 -0700

    updated cass seed with start and end

--- database-primary/seedPhotos.js
+++ database-primary/seedPhotos.js
@@ -9,16 +9,16 @@ const randomNumber = () => (
   Math.floor(Math.random() * 1000 + 1)
 );
 
-const seedPhotoData = () => {
+const seedPhotoData = (start, end) => {
   writer.pipe(fs.createWriteStream('csv-cql/photoData.csv'));
 
-  let count = 101;
+  let count = start;
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = start; i <= end; i++) {
     for (let j = 0; j < 20; j++) {
       writer.write({
         photoID: count++,
-        hotelID: i + 1,
+        hotelID: i,
         date: faker.date.between('2020-04-01', '2020-07-01'),
         photoAlbums: '[' + [photoAlbums[0], photoAlbums[Math.floor(Math.random() * 8 + 1)]] + ']',
         url: `https://sdc-carousel-photos.s3-us-west-1.amazonaws.com/photos/sdc-${randomNumber()}.jpg`
@@ -30,4 +30,4 @@ const seedPhotoData = () => {
   console.log('photo seed done');
 };
 
-seedPhotoData();
+seedPhotoData(1, 5);
