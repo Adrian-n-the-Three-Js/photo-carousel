dfab797e8d00eb1792c3680e1735a0e5b6e7c419
Adrian <vent.adrian@gmail.com>
2020-07-14 01:38:18 -0700

    seed scripts complete

--- database-primary/seedHotelPhotos.js
+++ database-primary/seedHotelPhotos.js
@@ -5,24 +5,22 @@ const faker = require('faker');
 const writer = csvWriter();
 
 const photoAlbums = ['All photos', 'Traveler', 'Hotel & Amenities', 'Room/Suite', 'Bathroom', 'Dining', 'Favorites', 'Pool & Beach', 'Family/Play Areas'];
-
 const randomNumber = () => (
   Math.floor(Math.random() * 1000 + 1)
 );
 
 const seedPhotoData = (start, end) => {
-  writer.pipe(fs.createWriteStream('csv-psql/photoData5.csv'));
+  writer.pipe(fs.createWriteStream('csv-cql/hotelPhotoData.csv'));
 
-  let idCount = start;
+  let photoIdCount = start;
 
   for (let i = start; i <= end; i++) {
     writer.write({
-      photoID: idCount++,
-      hotelID: Math.floor(idCount / 5) + 1,
+      hotelID: Math.floor(photoIdCount / 5) + 1,
+      photoID: photoIdCount++,
       date: faker.date.between('2020-04-01', '2020-07-01'),
-      photoAlbum1: photoAlbums[0],
-      photoAlbum2: photoAlbums[Math.floor(Math.random() * 8 + 1)],
-      url: `https://sdc-carousel-photos.s3-us-west-1.amazonaws.com/photos/sdc-${randomNumber()}.jpg`,
+      photoAlbums: `[${[photoAlbums[0], photoAlbums[Math.floor(Math.random() * 8 + 1)]]}]`,
+      url: `https://sdc-carousel-photos.s3-us-west-1.amazonaws.com/photos/sdc-${randomNumber()}.jpg`
     });
   }
 
--- database-secondary/schema.sql
+++ database-secondary/schema.sql
@@ -2,11 +2,11 @@ CREATE DATABASE sdc;
 
 \connect sdc;
 
-CREATE TABLE photos (
+CREATE TABLE hotel_photos (
   photoID SERIAL PRIMARY KEY,
   hotelID INTEGER NOT NULL,
   date VARCHAR(255) NOT NULL,
-  photoAlbum1: VARCHAR(255) NOT NULL,
-  photoAlbum2: VARCHAR(255),
+  photoAlbum1 VARCHAR(255) NOT NULL,
+  photoAlbum2 VARCHAR(255),
   url VARCHAR(255) NOT NULL
 );
